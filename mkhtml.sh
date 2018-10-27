a158df406964739f897697dcd8b0e11b2cf0928d
John C Good <jcg@ipac.caltech.edu>
2018-10-27 14:44:26 -0700

    Initial commit of Jupyter notebooks and HTML versions.

---
+++ mkhtml.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+jupyter nbconvert CSVTable.ipynb
+jupyter nbconvert mAddCube.ipynb
+jupyter nbconvert mAdd.ipynb
+jupyter nbconvert mArchiveExec.ipynb
+jupyter nbconvert mArchiveGet.ipynb
+jupyter nbconvert mArchiveList.ipynb
+jupyter nbconvert mBackground.ipynb
+jupyter nbconvert mBestImage.ipynb
+jupyter nbconvert mBgExec.ipynb
+jupyter nbconvert mBgModel.ipynb
+jupyter nbconvert mCoverageCheck.ipynb
+jupyter nbconvert mDiffExec.ipynb
+jupyter nbconvert mDiffFitExec.ipynb
+jupyter nbconvert mDiff.ipynb
+jupyter nbconvert mExamine.ipynb
+jupyter nbconvert mFitExec.ipynb
+jupyter nbconvert mFitplane.ipynb
+jupyter nbconvert mFixNaN.ipynb
+jupyter nbconvert mGetHdr.ipynb
+jupyter nbconvert mHdr.ipynb
+jupyter nbconvert mHistogram.ipynb
+jupyter nbconvert mImgtbl.ipynb
+jupyter nbconvert mJupyter.ipynb
+jupyter nbconvert mMakeHdr.ipynb
+jupyter nbconvert mMakeImg.ipynb
+jupyter nbconvert Mosaic.ipynb
+jupyter nbconvert Mosaic_oneshot.ipynb
+jupyter nbconvert mOverlaps.ipynb
+jupyter nbconvert mProjectCube.ipynb
+jupyter nbconvert mProject.ipynb
+jupyter nbconvert mProjectPP.ipynb
+jupyter nbconvert mProjectQL.ipynb
+jupyter nbconvert mProjExec.ipynb
+jupyter nbconvert mShrinkCube.ipynb
+jupyter nbconvert mShrink.ipynb
+jupyter nbconvert mSubCube.ipynb
+jupyter nbconvert mSubimage.ipynb
+jupyter nbconvert mSubset.ipynb
+jupyter nbconvert mTANHdr.ipynb
+jupyter nbconvert mTranspose.ipynb
+jupyter nbconvert mViewer.ipynb
+jupyter nbconvert mViewer_old.ipynb
+
+mv *.html html
